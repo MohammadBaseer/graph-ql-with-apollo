7a6f1ba8412bcacddeef8b054b2db53807fc2b2e
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-17 13:10:42 +0200

    Apollo Context Created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/navbar/Navbar";
+import { ApolloWrapper } from "@/lib/ApolloWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <Navbar />
-        {children}
+        <ApolloWrapper>{children}</ApolloWrapper>
       </body>
     </html>
   );
